feb0c844477493aaaea72a3db6a4a792c6e54577
Heo <130041734+Shelden-Hao@users.noreply.github.com>
2025-03-10 19:01:35 +0800

    修改阿里云文件存储服务信息

--- src/main/java/com/heo/utils/AliOssUtil.java
+++ src/main/java/com/heo/utils/AliOssUtil.java
@@ -15,8 +15,8 @@ public class AliOssUtil {
     private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
     // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
     //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-    private static final String ACCESS_KEY_ID = "LTAI5tBa8YDMFQzCki8gdoZo";
-    private static final String ACCESS_KEY_SECRET = "mRIxCAncj9jHm8JDDVL5fQ1kvomOdv";
+    private static final String ACCESS_KEY_ID = "xxxx"; // 改为自己的 ACCESS_KEY_ID
+    private static final String ACCESS_KEY_SECRET = "xxxx"; // 改为自己的 ACCESS_KEY_SECRET
     // 填写Bucket名称，例如examplebucket。
     private static final String BUCKET_NAME = "heoleadnews";
 
