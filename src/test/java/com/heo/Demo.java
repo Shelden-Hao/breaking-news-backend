3986035b52700a300f242865a152ef82a4be06f8
Heo <130041734+Shelden-Hao@users.noreply.github.com>
2025-03-10 19:03:00 +0800

    修改测试代码中的阿里云文件存储信息

--- src/test/java/com/heo/Demo.java
+++ src/test/java/com/heo/Demo.java
@@ -17,8 +17,8 @@ public class Demo {
         String endpoint = "https://oss-cn-beijing.aliyuncs.com";
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
         //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
-        String ACCESS_KEY_ID="LTAI5tBa8YDMFQzCki8gdoZo";
-        String ACCESS_KEY_SECRET="mRIxCAncj9jHm8JDDVL5fQ1kvomOdv";
+        String ACCESS_KEY_ID="xxxx"; // 改为自己的ACCESS_KEY_ID
+        String ACCESS_KEY_SECRET="xxxx"; // 改为自己的ACCESS_KEY_SECRET
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "heoleadnews";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
